5a6b7f9b532b39188c5cd62f4c4a64e27afae3fc
Makar <makar.papca@gmail.com>
2022-05-03 22:18:40 +0300

    Added client procedures and roles

---
+++ ClientProcedures.sql
@@ -0,0 +1,14 @@
+﻿CREATE OR REPLACE PACKAGE GamesClientPackage
+IS
+    PROCEDURE AddOrder(pGame Orders.GAME%TYPE, pClient Orders.CLIENT%TYPE, pStaff Orders.STAFF%TYPE, pDate Orders.ORDER_DATE%TYPE);
+END;
+
+CREATE OR REPLACE PACKAGE BODY GamesClientPackage
+IS
+    PROCEDURE AddOrder(pGame Orders.GAME%TYPE, pClient Orders.CLIENT%TYPE, pStaff Orders.STAFF%TYPE, pDate Orders.ORDER_DATE%TYPE) IS
+    BEGIN
+        GamesAdminPackage.AddOrder(pGame, pClient, pStaff, pDate);
+        EXCEPTION WHEN OTHERS THEN DBMS_OUTPUT.put_line(sqlerrm);
+    END;
+END;
+
