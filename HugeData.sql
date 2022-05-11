0b04aa6cd6e050563aae9c0308341606d207a039
Makar <makar.papca@gmail.com>
2022-05-11 19:05:21 +0300

    Finished

--- HugeData.sql
+++ HugeData.sql
@@ -1,7 +1,7 @@
-DECLARE
+CREATE OR REPLACE PROCEDURE RANDOM_DATA(length int) AS
     I INT := 0;
 BEGIN
-    FOR I IN 1..10000 LOOP
+    FOR I IN 1..length LOOP
         GamesAdminPackage.AddGame(
 dbms_random.value(1, 4),
         dbms_random.value(1, 4),
@@ -11,8 +11,12 @@ dbms_random.value(1, 4),
         );
     END LOOP;
 END;
-SELECT * FROM GAMES;
---SEQ_SCAN (TABLE ACCESS FULL)	 table: GAMES;	19407	109.0	cpu_cost = 7650476, io_cost = 108
 
-CREATE INDEX games_idx ON GAMES(ID, SUPPLIER, AUTHOR, GAME_TYPE, NAME, COST);
---SEQ_SCAN (TABLE ACCESS FULL)	 table: GAMES;	11335	108.0	cpu_cost = 5632476, io_cost = 108
+BEGIN
+    RANDOM_DATA(100000);
+end;
+
+SELECT * FROM GAMES;
+
+CREATE INDEX games_idx ON GAMES(ID, SUPPLIER, AUTHOR, GAME_TYPE, NAME, COST) ;
+drop index games_idx;
